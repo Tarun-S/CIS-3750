b55256bd2fcb31e037c06fcbca2cb8720b2d45fb
wmalcolm94 <wmalcolm94@outlook.com>
2016-11-19 12:50:02 -0500

    stuff

--- src/edit/editcrop.js
+++ src/edit/editcrop.js
@@ -1,4 +1,26 @@
 function addCrop()
 {
-    var form = 0//get id, then get, form, and add div with new label and input for crop.  Should come up woith like a delete button or something;
+    var head = document.getElementById("croplist");
+    //find div
+    var div = document.createElement('div');
+
+    //add text to div
+    var label = document.createElement("Label");
+    var input = document.createElement("Input");
+
+// <label class="control-label" for="crop">Crop</label>
+//                   <input class="form-control" id="crop" placeholder="Enter crop" type="text">
+
+    label.setAttribute("for", "input");
+    label.className = "control-label";
+    label.innerHTML = "Crop";
+    div.appendChild(label);
+    input.className = "form-control";
+    input.id = "crop";
+    input.setAttribute("placeholder", "Enter crop");
+    input.setAttribute("type", "text");
+    div.appendChild(input);
+
+    //append div to form
+    head.appendChild(div);
 }
