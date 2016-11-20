61cdba505632fad6ed540d2751bb8ba5ce889990
wmalcolm94 <wmalcolm94@outlook.com>
2016-11-20 14:39:42 -0500

    fixed the dynamic drop down

--- signup/signup.controller.js
+++ signup/signup.controller.js
@@ -5,107 +5,42 @@
         .module('app')
         .controller('SignupController', SignupController);
 
-    SignupController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
-    function SignupController(UserService, $location, $rootScope, FlashService) {
-        var vm = this;
-
-        vm.register = register;
-
-        function register() {
-            vm.dataLoading = true;
-            UserService.Create(vm.user)
-                .then(function (response) {
-                    if (response.success) {
-                        FlashService.Success('Registration successful', true);
-                        $location.path('/login');
-                    } else {
-                        FlashService.Error(response.message);
-                        vm.dataLoading = false;
-                    }
-                });
-        }
-
-        function filterDistricts(a, b) 
+    SignupController.$inject = ['$scope'];
+    function SignupController($scope) 
+    {
+        var northern = ['', 'Chitipa', 'Karonga', 'Likoma', 'Mzimba', 'Nkhata Bay', 'Rumphi'];
+        var central = ['', 'Dedza', 'Dowa', 'Kasungu', 'Lilongwe', 'Mchinji', 'Nkhotakota', 'Ntcheu', 'Ntchisi', 'Salima'];
+        var southern = ['', 'Balaka', 'Blantyre', 'Chikwawa', 'Chiradzulu', 'Machinga', 'Mangochi', 'Mulanje', 'Mwanza', 'Nsanje', 'Thyolo', 'Phalombe', 'Zomba', 'Neno'];
+  
+        $scope.selectedRegion = null;
+        $scope.selectedDistrict = null;   
+        
+        $scope.getRegions = function()
         {
-            var region = document.getElementById("region");
-            var district = document.getElementById("district");
-    
-            if (region != null)
-            {
-                clearCombo(district);
-                var str = region.options[region.selectedIndex].value;
-                if (str == "northern")
-                {
-                    list = ['',
-                            'Chitipa',
-                            'Karonga',
-                            'Likoma',
-                            'Mzimba',
-                            'Nkhata Bay',
-                            'Rumphi'];
-                }
-                else if (str == "central")
-                {
-                    list = ['',
-                            'Dedza',
-                            'Dowa',
-                            'Kasungu',
-                            'Lilongwe',
-                            'Mchinji',
-                            'Nkhotakota',
-                            'Ntcheu',
-                            'Ntchisi',
-                            'Salima'];
-                }
-                else if (str == "southern")
-                {
-                    list = ['',
-                            'Balaka',
-                            'Blantyre',
-                            'Chikwawa',
-                            'Chiradzulu',
-                            'Machinga',
-                            'Mangochi',
-                            'Mulanje',
-                            'Mwanza',
-                            'Nsanje',
-                            'Thyolo',
-                            'Phalombe',
-                            'Zomba',
-                            'Neno'];
-                }
-                else 
-                {
-                    list = []
-                }
-                addList(list);
-            }
-        }
-
-        function addList(list)
+            var list = ['', 'Central', 'Northern', 'Southern'];
+            return list
+        };
+  
+        $scope.filterDistricts = function()
         {
-            for (var i = 0; i < list.length; i++)
+            var list = []
+            if ($scope.selectedRegion !== '')
             {
-                var option = document.createElement('option');
-                option.text = option.value = list[i];
-                try {
-                    district.add(option, null); //Standard
-                } catch(error) {
-                    district.add(option); // IE only
+                if ($scope.selectedRegion == "Northern")
+                {
+                    list = northern;
+                }
+                else if ($scope.selectedRegion == "Central")
+                {
+                    list = central;
+                }
+                else if ($scope.selectedRegion == "Southern")
+                {
+                    list = southern;
                 }
             }
-        }
-
-        function clearCombo(combo)
-        {
-            if (combo != null)
-            {
-                while (combo.options.length > 0) 
-                {                
-                    combo.remove(0);
-                }
-            }
-        }
-    }
+            return list;
+        };
+    }   
 
 })();
